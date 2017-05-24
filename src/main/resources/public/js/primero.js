0632c60ec9f8bee9a7af574574aef2bbcc916e34
Jr.Armando <Jr.Armando@10.20.3.197>
2017-05-23 21:43:20 -0500

    proyecto

---
+++ src/main/resources/public/js/primero.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//El siguiente es el equivalente a la clase system con out printline
+console.log("hola mundo");
+
+$("#boton1").click( function(){
+    
+    //console.log("Haz oprimido el boton");
+    $("#algo").html("Haz oprimido el boton");
+    }
+        );
+
