c80726293d80e9ee88f8238d6abba82afca1a83f
Jr.Armando <Jr.Armando@10.20.3.197>
2017-06-27 21:43:19 -0500

    proyecto

--- src/main/java/org/armando/bimestral/Boleto.java
+++ src/main/java/org/armando/bimestral/Boleto.java
@@ -5,32 +5,60 @@
  */
 package org.armando.bimestral;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import static jdk.nashorn.internal.runtime.Debug.id;
+
 /**
  *
  * @author Jr.Armando
  */
+@Entity
+@Table(name="boleto")
 public class Boleto {
-    private Integer id_boleto;
-    private Integer id_sala;
+
+    @Id
+    @GeneratedValue
+    @Column
+    private Long id_boleto;
+    @Column
+    private Long id_sala;
+    @Column
     private float costoBoleto;
+    
+    @ManyToOne
+    //foreign key
+    @JoinColumn(name="id_sala")
+    SalaCine salaCine;
+
+    @Override
+    public String toString() {
+        return "Boleto{" + "id_boleto=" + id_boleto + ", id_sala=" + id_sala + ", costoBoleto=" + costoBoleto + ", salaCine=" + salaCine + '}';
+    }
+    
 
     public Boleto() {
     }
     
 
-    public Integer getId_boleto() {
+    public Long getId_boleto() {
         return id_boleto;
     }
 
-    public void setId_boleto(Integer id_boleto) {
+    public void setId_boleto(Long id_boleto) {
         this.id_boleto = id_boleto;
     }
 
-    public Integer getId_sala() {
+    public Long getId_sala() {
         return id_sala;
     }
 
-    public void setId_sala(Integer id_sala) {
+    public void setId_sala(Long id_sala) {
         this.id_sala = id_sala;
     }
 
@@ -42,7 +70,7 @@ public class Boleto {
         this.costoBoleto = costoBoleto;
     }
 
-    public Boleto(Integer id_boleto, Integer id_sala, float costoBoleto) {
+    public Boleto(Long id_boleto, Long id_sala, float costoBoleto) {
         this.id_boleto = id_boleto;
         this.id_sala = id_sala;
         this.costoBoleto = costoBoleto;
--- src/main/java/org/armando/bimestral/SalaCine.java
+++ src/main/java/org/armando/bimestral/SalaCine.java
@@ -5,32 +5,51 @@
  */
 package org.armando.bimestral;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Jr.Armando
  */
+@Entity
+@Table(name="salacine")
+
 public class SalaCine {
     
-    private Integer id_sala;
+    @Id
+    @Column
+    private Long id_sala;
+    
+    @Column(name="tituloPelicul")
     private String tituloPelicula;
+    @Column(name="clasificacion")
     private String clasificacion;
+    @Column(name="numeroAsientos")
     private Integer numeroAsientos;
+
+    @Override
+    public String toString() {
+        return "SalaCine{" + "id_sala=" + id_sala + ", tituloPelicula=" + tituloPelicula + ", clasificacion=" + clasificacion + ", numeroAsientos=" + numeroAsientos + '}';
+    }
 
     public SalaCine() {
     }
 
-    public SalaCine(Integer id_sala, String tituloPelicula, String clasificacion, Integer numeroAsientos) {
+    public SalaCine(Long id_sala, String tituloPelicula, String clasificacion, Integer numeroAsientos) {
         this.id_sala = id_sala;
         this.tituloPelicula = tituloPelicula;
         this.clasificacion = clasificacion;
         this.numeroAsientos = numeroAsientos;
     }
 
-    public Integer getId_sala() {
+    public Long getId_sala() {
         return id_sala;
     }
 
-    public void setId_sala(Integer id_sala) {
+    public void setId_sala(Long id_sala) {
         this.id_sala = id_sala;
     }
 
